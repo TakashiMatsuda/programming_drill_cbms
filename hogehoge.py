35522b10ffa241b7101ad0fb52213406ee1a697d
Takashi Matsuda <takashi.mtsd@gmail.com>
2019-05-17 16:34:06 +0900

    add class example

---
+++ hogehoge.py
@@ -0,0 +1,7 @@
+class Hogehoge:
+    def __init__(self, x):
+        self.x = x
+
+    def plus(self, value):
+        return value + self.x
+
---
+++ mathconst.py
@@ -0,0 +1,4 @@
+class Mathconst:
+    PI = 3.14159
+    e = 2.71828
+
