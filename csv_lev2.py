6f13602ffa343c9f842d07b8f1211e25a7b22812
Takashi Matsuda <takashi.mtsd@gmail.com>
2019-05-17 11:43:16 +0900

    remove quote

--- csv_lev2.py
+++ csv_lev2.py
@@ -3,6 +3,18 @@
 import re
 
 csvpattern = re.compile(r"((\"[[\x00-\x7F]+?\"|[^,]+?),)+?(\"[[\x00-\x7F]+\"|[^,]+)$")
-target = input()
-print(csvpattern.sub(r'\2\t\3', target))
-
+quotepattern = re.compile(r"\"[[\x00-\x7F]+?\"")
+while True:
+    try:
+        line = input()
+    except Exception:
+        break
+    matches = csvpattern.match(line)
+    row = matches.group(2, 3)
+    row = [x[1:-1] if quotepattern.match(x) else x for x in row]
+    print("\t".join(row))
+#
+#    id_ = csvpattern.match(line, 3)
+#    name = name[1:-1] if quotepattern.match(name) else name
+#    id_ = id_[1:-1] if quotepattern.match(id_) else id_
+#    print("\t".join([name, id_]))
